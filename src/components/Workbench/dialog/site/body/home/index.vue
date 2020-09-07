753871904578801a86ed0752ab44d02d67dd3739
Funny <2048412928@qq.com>
2020-09-07 13:25:44 +0800

    update
    update

---
+++ src/components/Workbench/dialog/site/body/home/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="site-home">
+    <el-divider content-position="left"><h3>{{ $t('home') }}</h3></el-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "site-home",
+  mounted() {
+    this.$nextTick(() => {
+      const menuData = [
+        {isMenu: false, name: 'home', lang: 'home'},
+      ]
+      this.$emit('menuCallback', menuData)
+    })
+  }
+}
+</script>
+
+<style lang="less" scoped>
+/deep/
+.el-divider__text {
+  background-color: #f2f2f2;
+}
+</style>
+
+<i18n src="./lang.json"/>
--- src/components/Workbench/dialog/site/index.vue
+++ src/components/Workbench/dialog/site/index.vue
@@ -4,18 +4,20 @@
       <div class="menu-list"
            :key="item.lang"
            v-for="item in menu.list"
-           :class="{'menu-list-active':item.lang===menu.key}"
+           :class="{'menu-list-active': item.name === menu.key}"
       >
         <div :class="{'menu-list-menu':item.isMenu}" @click="listClick(item)">{{ $t(item.lang) }}</div>
       </div>
 
     </div>
     <div class="workbench-site-body">
-      <div style="display: flex; align-items: center;">
+      <div class="workbench-site-home" style="display: flex; align-items: center;">
         <h3 style="margin-right: auto;">{{ $t('langSite') }}</h3>
-        <lang-module/>
+        <div>
+          <lang-module/>
+        </div>
       </div>
-      <component v-for="item in component.list" :key="item.name" :is="item" @menuCallback="menuCallback"/>
+      <component v-for="item in componentList" :key="item.name" :is="item" @menuCallback="menuCallback"/>
     </div>
   </div>
 </template>
@@ -28,9 +30,8 @@ export default {
   name: "workbench-menu-site",
   data: function () {
     return {
-      component: {
-        list: []
-      },
+      viewList: ['home'],
+      componentList: [],
       menu: {
         key: null,
         list: [
@@ -43,18 +44,30 @@ export default {
   methods: {
     // 回调填充
     menuCallback(data) {
-      this.menu.list.push(data);
-      if (!this.menu.key && !data.isMenu) {
-        this.listClick(data)
+      // 填充到菜单
+      this.menu.list = (this.menu.list).concat(...data)
+      for (const valData of data) {
+        // 菜单列表无数据
+        if (!this.menu.key && !valData.isMenu) {
+          this.listClick(valData)
+        }
       }
     },
     // 菜单点击
-    listClick({lang: name, path = null}) {
-      if (path) {
+    listClick({name, isMenu}) {
+      if (!isMenu) {
         this.menu.key = name
       }
       // document.querySelector("#header").scrollIntoView(true);
-      console.log(path)
+      // console.log(path)
+    }
+  },
+  mounted() {
+    // 获取需要加载的列表
+    const viewList = [this.viewList]
+    for (const valPath of viewList) {
+      // 填充至 模板列表
+      this.componentList.push(() => import(/* webpackChunkName: "WorkbenchDialog" */  `./body/${valPath}/index.vue`))
     }
   }
 }
